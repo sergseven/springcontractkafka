0f1874c156438e6b3f8356be4db87541f3c8e924
sergey.nesprava <sergey.nesprava@globallogic.com>
2019-06-28 21:33:08 +0200

    Add producer to producer module

--- producer/src/main/java/producer/ProducerApplication.java
+++ producer/src/main/java/producer/ProducerApplication.java
@@ -1,13 +1,34 @@
 package producer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
+@Slf4j
 public class ProducerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ProducerApplication.class, args);
+  }
+
+  @Autowired
+  private KafkaTemplate<String, String> template;
+
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    return args -> {
+      log.info("App started.");
+
+      template.send("springcontracttest", "foo2");
+
+      log.info("Message(s) sent.");
+    };
   }
 
 }
--- producer/src/main/java/producer/config/KafkaProducerConfig.java
+++ producer/src/main/java/producer/config/KafkaProducerConfig.java
@@ -1,19 +1,21 @@
 package producer.config;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 @EnableKafka
 @Slf4j
 public class KafkaProducerConfig {
 
-  @KafkaListener(topics = "springcontracttest")
-  public void listen(@Payload String message) {
-    log.info("Received Messasge in group - group-id: %s", message);
-  }
+
 
 }
